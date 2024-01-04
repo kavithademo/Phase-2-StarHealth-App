f223673b0277ab7ad2baf19c10dc35216f41d201
kavitha <vkavitha434@gmail.com>
2024-01-04 06:27:50 +0530

    Project done

---
+++ src/main/java/com/simplylearn/bddtest/Phase_2_StarHealth_App/App.java
@@ -0,0 +1,13 @@
+package com.simplylearn.bddtest.Phase_2_StarHealth_App;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/simplylearn/bddtest/testrunner/TestRunner.java
@@ -0,0 +1,13 @@
+package com.simplylearn.bddtest.testrunner;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/Features/starhealth.feature", 
+	glue = "com/simplilearn/bddtest/stepdefinitions",
+	plugin = "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:")
+public class TestRunner {
+
+}
